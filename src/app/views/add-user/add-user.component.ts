2b9dc8e4aa0906e87c740345d41ada40af6ebf06
Gabriele <ruggzan@gmail.com>
2022-03-08 12:43:55 +0100

    gedtione utenti

--- src/app/views/add-user/add-user.component.ts
+++ src/app/views/add-user/add-user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedAnimations } from 'src/app/shared/animations/shared-animations';
-import { FormBuilder, FormGroup, PatternValidator, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, PatternValidator, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
@@ -8,6 +8,8 @@ import { ToastrService } from 'ngx-toastr';
 // import custom validator to validate that password and confirm password fields match
 import { MustMatch } from 'src/app/_helpers/must-match.validator';
 import { environment } from 'src/environments/environment';
+import { Azienda } from 'src/app/model/Azienda';
+import { GenerateResponse } from 'src/app/model/GenerateResponse';
 
 @Component({
   selector: 'app-add.user',
@@ -18,9 +20,12 @@ import { environment } from 'src/environments/environment';
 export class AddUserComponent implements OnInit {
 
   loading: boolean;
-  loadingText: string = "Registrati";
-  signupForm: FormGroup;
+  loadingText: string = "Registrazione";
+  addUserForm: FormGroup;
   submitted = false;
+  listAzienda : Azienda[] = [];
+
+
   headerDict = {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
@@ -43,63 +48,86 @@ export class AddUserComponent implements OnInit {
 
     this.loading = false;
 
-    this.signupForm = this.fb.group({      
+    this.addUserForm = this.fb.group({      
       email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
       username: ['', Validators.required],
-      role: this.roles,
+      azienda: [null, Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', Validators.required]
     }, {
       validator: MustMatch('password', 'confirmPassword')
     });
+
+    this.getListaAziende();
+
+
+  }
+
+  getListaAziende(){
+      this.http.get<Azienda[]>(`${environment.apiUrl}getListAzienda`)
+      .subscribe(
+        res =>{
+          console.log(res);
+          if (res.length!==0) {
+            this.listAzienda= res;
+          } else {
+            this.toastr.error('Nessun Azienda trovata!','Azienda',{progressBar: false});
+          }
+        },
+        err =>{
+          console.log(err);        }
+      )
+  }
+
+  compareAzienda(a: Azienda, b: Azienda) {
+    return a && b && a.id === b.id;
   }
 
   // convenience getter for easy access to form fields
-  get f() { return this.signupForm.controls; }
+  get f() { return this.addUserForm.controls; }
 
   onSubmit() {
-    
     this.submitted = true;
-
     // stop here if form is invalid
-    if (this.signupForm.invalid) {
+    if (this.addUserForm.invalid) {
       return;
     }
     this.loading = true;
     this.loadingText = 'Registrazione in corso...';
-    this.registMovarish();   
+    this.conferma();   
+
   }
 
   checkPasswords(group: FormGroup) { // here we have the 'passwords' group
     const password = group.get('password').value;
     const confirmPassword = group.get('confirmpassword').value;
-
     return password === confirmPassword ? null : { notSame: true }
   }
 
   
-  signup() {
-    this.loading = true;
-    this.loadingText = 'Registrazione in corso...';
-    this.registMovarish();
-  }
+  // signup() {
+  //   this.loading = true;
+  //   this.loadingText = 'Registrazione in corso...';
+  //   this.registMovarish();
+  // }
 
 
 
-  registMovarish() {
-    this.roles.push("user");
-    console.log(this.signupForm.value);
-    this.http.post<any>(`${environment.apiUrl}auth/signup`, JSON.stringify(this.signupForm.value), this.requestOptions).subscribe(
-      res => {
-        console.log(res);
-        this.toastr.success('Registrazione.', 'Registrazione avvenuta con successo', { progressBar: false });
-        this.router.navigateByUrl('/sessions/signin');
-      }, err => {
-        this.toastr.error('Registrazione.', "Si è verificato un errore o l'utente è già registrato");
-        this.loading = false;
-        this.loadingText = 'Registrati';
-        console.log(err);
-      })
+  conferma() {
+      console.log(this.addUserForm.value);
+      this.http.post<GenerateResponse>(`${environment.apiUrl}auth/addUser`, JSON.stringify(this.addUserForm.value), this.requestOptions).subscribe(
+   // this.http.post<GenerateResponse>(`${environment.apiUrl}generateAzienda`, azienda).subscribe(res => {
+        res => {
+          console.log(res);
+          this.toastr.success('Aggiungi Utente.', 'Utente inserito con successo', { progressBar: false });
+          // this.router.navigateByUrl('/sessions/signin');
+          this.loading = false;
+        }, err => {
+          this.toastr.error('Aggiungi Utente.', err.error.message);
+          this.loading = false;
+          this.loadingText = 'Aggiungi Utente';
+          console.log(err);
+        })
   }
 
 }
